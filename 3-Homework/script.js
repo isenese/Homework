135af35ed9089ad90a3c0768b65967d330f35281
alvaro nieto <alvaro16nieto@gmail.com>
2020-03-10 15:44:01 +1030

    New update. Now the password genrator works

--- 3-Homework/script.js
+++ 3-Homework/script.js
@@ -1,113 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// Fuctions for generate ramdom values
 
-  ////////////////////////////////
-  var length;
-  function promptLength () {
-
-    length = prompt("How long do you need your password to be, between 8 and 128 characters");
-
-    if ( length <= 8  || length > 128 ) {
-      alert("the number chosen is not a valid option")
-    } else {
-
-        return 
-    }
-  }
-  promptLength()
-
-//   var upperCase;
-  
-//   function promptUppercase () {
-
-//     upperCase = prompt("Do you want uppercase characters, please answer 'yes' or 'no' ");
-
-//     if ( upperCase === 'yes'  || upperCase === 'YES' || upperCase === 'no' || upperCase === 'NO' ) {
-//         return
-//     } else {
-//              alert('your option is not valid, check your spelling')
-//     }
-//     }
-
-//     promptUppercase()
-
-//     var lowerCase;
-
-//     function promptLowercase () {
-
-//         lowerCase = prompt("Do you want lowercase characters, please answer 'yes' or 'no' ");
-
-//     if ( lowerCase === 'yes'  || lowerCase === 'YES' ) {
-//         return
-//     } else if ( lowerCase === 'no' || lowerCase === 'NO' ) {
-//         return
-//     } else {
-//              alert('your option is not valid, check your spelling')
-//     }
-//     }
-
-//     promptLowercase()
-
-//     var symbol;
-
-//     function promptSymbol () {
-
-//         symbol = prompt("Do you want symbol characters, please answer 'yes' or 'no' ");
-
-//         if ( symbol === 'yes'  || symbol === 'YES' ) {
-//             return
-//         } else if ( symbol === 'no' || symbol === 'NO' ) {
-//             return
-//         } else {
-//                 alert('your option is not valid, check your spelling')
-//         }
-//     }
-
-//     promptSymbol()
-
-
-//   console.log(length)
-//   console.log(upperCase)
-//   console.log(lowerCase)
-//   console.log(symbol)
-
-function generatePassword (){ 
-    var myFunctions = [getRandomLower(), getRandomNumber() , getRandomUpper(), getRandomSymbol(),  ]
-
-function randomNumber () {
-    return Math.floor(Math.random()* Math.floor (4));
-}
-
-// console.log (randomNumber());
-
-    var result = [] 
-
-    for (i=0; i < length; i++){
-        result.push(myFunctions[randomNumber()]);
-    }
-console.log (result)
-}
-var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-  
-//   passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// function generatePassword(){
-// }
-
-
-
-// Fuctions
 function getRandomLower() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -127,10 +22,127 @@ function getRandomSymbol() {
 }
 
 
+// Write password to the #password input
+function writePassword() {
 
-///////////////////////////
+  var length;
+  function promptLength () {
+
+    length = prompt("How long do you need your password to be, between 8 and 128 characters");
+
+    if ( length <= 8  || length > 128 ) {
+      alert("the number chosen is not a valid option")
+    } else {
+
+        return 
+    }
+  }
+  promptLength()
+
+  var upperCase;
+  
+  function promptUppercase () {
+
+    upperCase = prompt("Do you want uppercase characters, please answer 'yes' or 'no' ");
+
+    if ( upperCase === 'yes'  || upperCase === 'YES' || upperCase === 'no' || upperCase === 'NO' ) {
+        return
+    } else {
+             alert('your option is not valid, check your spelling')
+    }
+    }
+
+    promptUppercase()
+
+    var lowerCase;
+
+    function promptLowercase () {
+
+        lowerCase = prompt("Do you want lowercase characters, please answer 'yes' or 'no' ");
+
+    if ( lowerCase === 'yes'  || lowerCase === 'YES' ) {
+        return
+    } else if ( lowerCase === 'no' || lowerCase === 'NO' ) {
+        return
+    } else {
+             alert('your option is not valid, check your spelling')
+    }
+    }
+
+    promptLowercase()
+
+    var symbols;
+
+    function promptSymbolCase () {
+
+        symbols = prompt("Do you want symbol characters, please answer 'yes' or 'no' ");
+
+        if ( symbols === 'yes'  || symbols === 'YES' ) {
+            return
+        } else if ( symbols === 'no' || symbols === 'NO' ) {
+            return
+        } else {
+                alert('your option is not valid, check your spelling')
+        }
+    }
+
+    promptSymbolCase()
+
+    var NumberCase;
+
+    function promptNumberCase () {
+
+        NumberCase = prompt("Do you want number characters, please answer 'yes' or 'no' ");
+
+        if ( NumberCase === 'yes'  || NumberCase === 'YES' ) {
+            return
+        } else if ( NumberCase === 'no' || NumberCase === 'NO' ) {
+            return
+        } else {
+                alert('your option is not valid, check your spelling')
+        }
+    }
+
+    promptNumberCase()
 
 
+// Password Generation
+function generatePassword (){ 
+    var myFunctions = []
+
+    if (lowerCase === "yes" || lowerCase === "YES"  ){
+        myFunctions.push(getRandomLower)
+    }
+    if (NumberCase === "yes" || NumberCase === "YES"  ){
+        myFunctions.push(getRandomNumber)
+    }
+    if (upperCase === "yes" || upperCase === "YES"  ){
+        myFunctions.push(getRandomUpper)
+    }
+    if (symbols === "yes" || symbols === "YES"  ){
+        myFunctions.push(getRandomSymbol)
+    }
 
 
+function randomNumber () {
+    return Math.floor(Math.random()* Math.floor (myFunctions.length));
+}
+
+    var result = [] 
+    
+    for (i=0; i < length-1; i++){
+        result.push(myFunctions[randomNumber()]())
+    }
+return result.join('')
+}
+var password = generatePassword();
+
+var passwordText = document.querySelector("#password");
+
+   passwordText.value = password;
+console.log(password)
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
